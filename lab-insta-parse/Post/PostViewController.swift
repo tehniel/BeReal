373020ecd52343d2ccb1adcb8ff96bd097c7ad10
tehniel <skydan.inc@gmail.com>
2023-04-19 12:19:43 -0400

    update for Unit 6

--- lab-insta-parse/Post/PostViewController.swift
+++ lab-insta-parse/Post/PostViewController.swift
@@ -88,6 +88,29 @@ class PostViewController: UIViewController {
                     print("✅ Post Saved! \(post)")
 
                     // TODO: Pt 2 - Update user's last posted date
+                    // Get the current user
+                    if var currentUser = User.current {
+
+                        // Update the `lastPostedDate` property on the user with the current date.
+                        currentUser.lastPostedDate = Date()
+
+                        // Save updates to the user (async)
+                        currentUser.save { [weak self] result in
+                            switch result {
+                            case .success(let user):
+                                print("✅ User Saved! \(user)")
+
+                                // Switch to the main thread for any UI updates
+                                DispatchQueue.main.async {
+                                    // Return to previous view controller
+                                    self?.navigationController?.popViewController(animated: true)
+                                }
+
+                            case .failure(let error):
+                                self?.showAlert(description: error.localizedDescription)
+                            }
+                        }
+                    }
 
 
                 case .failure(let error):
